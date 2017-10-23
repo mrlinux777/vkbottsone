157d82b37a5d56555a5852d2508ab7bde22411c4
mrlinux777 <danildavid207@gmail.com>
2017-10-23 18:37:35 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,97 +1,128 @@
-import traceback
+import datetime
+import time
 
 import hues
-
-from plugin_system import PluginSystem
-from vkplus import Message
+import peewee
+import peewee_async
 
 try:
-    import settings
-except ImportError:
-    pass
+    from settings import DATABASE_SETTINGS, DATABASE_DRIVER, DATABASE_CHARSET
+except:
+    DATABASE_SETTINGS, DATABASE_DRIVER, DATABASE_CHARSET = (), None, "utf8mb4"
+
+additional_values = {}
+if DATABASE_DRIVER == "mysql":
+    driver = peewee_async.MySQLDatabase
+    additional_values['charset'] = DATABASE_CHARSET
+elif DATABASE_DRIVER == "postgresql":
+    driver = peewee_async.PostgresqlDatabase
+else:
+    driver = None
+
+if len(DATABASE_SETTINGS) == 0:
+    database = False
+elif len(DATABASE_SETTINGS) == 1:
+    name, = DATABASE_SETTINGS
+    database = driver(name)
+else:
+    name, host, port, user, password = DATABASE_SETTINGS
+    database = driver(name,
+                      host=host,
+                      port=port,
+                      user=user,
+                      password=password,
+                      **additional_values)
 
 
-class Command(object):
-    __slots__ = ('has_prefix', 'text', 'bot',
-                 'command', 'args', "msg")
+async def get_or_none(model, *args, **kwargs):
+    try:
+        return await db.get(model, *args, **kwargs)
 
-    def __init__(self, msg: Message):
-        self.has_prefix = True  # переменная для обозначения, есть ли у команды префикс
-        self.msg = msg
-        self.text = msg.body
-        self._get_prefix()
-        self.command = ""
-        self.args = []
-        # Если команда пустая
-        if not self.text.strip():
-            self.has_prefix = False
+    except peewee.DoesNotExist:
+        return None
 
-    def check_command(self, command_system):
-        if not self.has_prefix:
-            return False
+async def set_up_roles(bot):
+    from settings import BLACKLIST, WHITELIST, ADMINS
 
-        for command in command_system.commands:
-            if self.text.startswith(command + " ") or self.text == command:
-                self.command = command
-                self.args = self.text.replace(command, "", 1).split()
-                self.msg.text = " ".join(self.args)
-                return True
+    if WHITELIST:
+        bot.WHITELISTED = True
 
-        if command_system.ANY_COMMANDS:
-            self.args = self.text.split()
-            self.msg.text = " ".join(self.args)
-            return True
+    for u in WHITELIST:
+        await db.get_or_create(Role, user_id=u, role="whitelisted")
 
-        return False
+    for u in BLACKLIST:
+        await db.get_or_create(Role, user_id=u, role="blacklisted")
 
-    def log(self):
-        """Пишет в лог, что была распознана команда"""
-        pid = self.msg.peer_id
-        who = ("конференции {}" if self.msg.conf else "ЛС {}").format(pid)
-        hues.info(f"Команда '{self.command}' из {who} с аргументами {self.args}")
+    for u in ADMINS:
+        await db.get_or_create(Role, user_id=u, role="admin")
 
-    def _get_prefix(self):
-        """Пытается получить префикс из текста команды"""
-        for prefix in settings.PREFIXES:
-            # Если команда начинается с префикса
-            if self.text.startswith(prefix):
-                # Убираем префикс из текста
-                self.text = self.text.replace(prefix, '', 1).lstrip()
-                self.msg.text = self.text
-                break
-
-        else:
-            self.has_prefix = False
+    await check_white_list(bot)
 
 
-class CommandSystem(object):
-    def __init__(self, commands, plugin_system: PluginSystem):
-        # Система плагинов
-        self.system = plugin_system
-        # self.commands - список с командами
-        self.commands = commands
-        self.ANY_COMMANDS = bool(plugin_system.any_commands)
+async def check_white_list(bot):
+    if await db.count(Role.select().where(Role.role == "whitelisted")) > 0:
+        bot.WHITELISTED = True
 
-    async def process_command(self, msg_obj: Message, cmd: Command):
-        """Обрабатывает команду"""
-        if not cmd.check_command(self):
-            return False
+    else:
+        bot.WHITELISTED = False
 
-        cmd_text = cmd.command
-        # Логгируем команду, если нужно (но не логгируем плагины,
-        # которые реагируют на любые команды)
-        if settings.LOG_COMMANDS and not self.ANY_COMMANDS:
-            cmd.log()
-        try:
-            await self.system.call_command(cmd_text, msg_obj, cmd.args)
 
-            return True
-        # Если в плагине произошла какая-то ошибка
-        except Exception:
-            await msg_obj.answer(f"{msg_obj.vk.anti_flood()}. "
-                                 f"Произошла ошибка при выполнении команды <{cmd_text}> "
-                                 "пожалуйста, сообщите об этом разработчику!")
-            hues.error(
-                f"Произошла ошибка при вызове команды '{cmd_text}' с аргументами {cmd.args}. "
-                f"Текст сообщения: '{msg_obj._data}'."
-                f"Ошибка:\n{traceback.format_exc()}")
+#############################################################################################
+class BaseModel(peewee.Model):
+    class Meta:
+        database = database
+
+
+class Role(BaseModel):
+    user_id = peewee.BigIntegerField()
+    role = peewee.TextField()
+    # blacklisted - юзер забанен
+    # whitelisted - юзер находится в белом списке
+    # admin - юзер является админом
+
+
+class User(BaseModel):
+    uid = peewee.BigIntegerField(primary_key=True, unique=True)
+    message_date = peewee.BigIntegerField(default=0)
+    in_group = peewee.BooleanField(default=False)
+
+    do_not_disturb = peewee.BooleanField(default=False)
+    memory = peewee.TextField(default="")
+
+    chat_data = peewee.TextField(default="")
+
+
+class Ignore(BaseModel):
+    ignored = peewee.ForeignKeyField(User, related_name='ignored_by')
+    ignored_by = peewee.ForeignKeyField(User, related_name='ignored')
+
+    class Meta:
+        indexes = (
+            (('ignored', 'ignored_by'), True),
+        )
+
+
+class ListForMail(BaseModel):
+    user_id = peewee.BigIntegerField(unique=True)
+    date = peewee.DateTimeField(default=datetime.datetime.now())
+
+
+class BotStatus(BaseModel):
+    last_top = peewee.TextField(default="")
+    mail_data = peewee.TextField(default="")
+
+    photos = peewee.IntegerField(default=0)
+    timestamp = peewee.IntegerField(default=time.time())
+
+
+if database:
+    db = peewee_async.Manager(database)
+
+    User.create_table(True)
+    Ignore.create_table(True)
+    ListForMail.create_table(True)
+    BotStatus.create_table(True)
+    Role.create_table(True)
+
+else:
+    hues.error("Не удалось создать базу данных! Проверьте настройки и попробуйте снова!")
